f60e0d6910145b1f53030cc0bc9193c0243ae65f
Dave Ames <david.john.ames@gmail.com>
2024-09-17 12:16:08 +0100

    Added all the parts to make a working system apart from application.yml

--- src/main/java/com/booleanuk/Main.java
+++ src/main/java/com/booleanuk/Main.java
@@ -1,11 +1,16 @@
 package com.booleanuk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
 }
